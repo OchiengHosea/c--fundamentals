dff25b3129924ac8c186c19850c239f8a59b6ce9
OchiengHosea <jrhosea8@gmail.com>
2021-12-24 20:24:45 +0300

    Collections

--- Gradebook/Program.cs
+++ Gradebook/Program.cs
@@ -8,8 +8,19 @@ namespace Gradebook
         {
             double x = 3.434;
             double y = 20.545;
-
             Console.WriteLine(x + y);
+
+            string[] passtimers = {"Music", "Movies", "Cards", "Checkers"};
+
+            //for(int i=0; i < passtimers.Length - 1; i++)
+            //{
+            //    Console.WriteLine(passtimers[i]);
+            //}
+
+            foreach(string passtimer in passtimers)
+            {
+                Console.WriteLine(passtimer);
+            }
         }
     }
 }
