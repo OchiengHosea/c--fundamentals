2d9ce9e48355beb6b52c9f437b603edceed8b28d
OchiengHosea <jrhosea8@gmail.com>
2021-12-24 20:05:37 +0300

    Basic starting

---
+++ Gradebook/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gradebook
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double x = 3.434;
+            double y = 20.545;
+
+            Console.WriteLine(x + y);
+        }
+    }
+}
